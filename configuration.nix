80c9d063e8a3077f1bd06fc6e59678c002df920d
Tomas Harkema <tomas@harkema.io>
2026-03-24 16:54:05 +0100

    fix 1password

--- configuration.nix
+++ configuration.nix
@@ -92,8 +92,15 @@ in {
   programs.geary.enable = true;
   environment.shells = [pkgs.zsh];
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = ["tomas"];
+  };
+
   environment.systemPackages = with pkgs; [
-    _1password-gui
     alejandra
     apple-cursor
     bottom
