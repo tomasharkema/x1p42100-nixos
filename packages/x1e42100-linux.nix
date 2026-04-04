1005c1ea2d49dc1aa08e89574d9190562ab1ae88
Tomas Harkema <tomas@harkema.io>
2026-04-04 20:39:13 +0200

    use 6.19.6

--- packages/x1e42100-linux.nix
+++ packages/x1e42100-linux.nix
@@ -16,37 +16,14 @@
 #     hash = "sha256-PLQaIKZ6Kcwf56CgxNQg45EilTOi6xnjgPjP6BsTcgM=";
 #   };
 # })
-# linuxPackagesFor (
-#   (buildLinux {
-#     version = "6.19.6";
-#     src = fetchFromGitHub {
-#       owner = "jglathe";
-#       repo = "linux_ms_dev_kit";
-#       rev = "jg/ubuntu-qcom-x1e-6.19.y";
-#       hash = "sha256-ji2IlIm3dyO0UdhVMW9gmBQ93ZjwjO84nH5Bh6zsJt8=";
-#     };
-#     kernelPatches = [
-#       {
-#         name = "gamma";
-#         patch = fetchpatch {
-#           url = "https://patchwork.kernel.org/project/linux-arm-msm/patch/20251018-dpu-add-dspp-gc-driver-v1-1-ed0369214252@izzo.pro/mbox/";
-#           sha256 = "sha256-ez9nsrfhKGiP/YB9LsyRISYPDK1l9G8aqjnacCltQDE=";
-#         };
-#       }
-#       {
-#         name = "mac-address";
-#         patch = ./mac-address.patch;
-#       }
-#     ];
-#   })
 linuxPackagesFor (
   (buildLinux {
-    version = "6.19";
+    version = "6.19.6";
     src = fetchFromGitHub {
       owner = "jglathe";
       repo = "linux_ms_dev_kit";
-      rev = "jg/sp12_dt";
-      hash = "sha256-dcS5AHP40Saz21ZilNFYX4WtgYf404z+setpywvsopw=";
+      rev = "jg/ubuntu-qcom-x1e-6.19.y";
+      hash = "sha256-ji2IlIm3dyO0UdhVMW9gmBQ93ZjwjO84nH5Bh6zsJt8=";
     };
     kernelPatches = [
       {
@@ -62,6 +39,29 @@ linuxPackagesFor (
       }
     ];
   })
+# linuxPackagesFor (
+#   (buildLinux {
+#     version = "6.19";
+#     src = fetchFromGitHub {
+#       owner = "jglathe";
+#       repo = "linux_ms_dev_kit";
+#       rev = "jg/sp12_dt";
+#       hash = "sha256-dcS5AHP40Saz21ZilNFYX4WtgYf404z+setpywvsopw=";
+#     };
+#     kernelPatches = [
+#       {
+#         name = "gamma";
+#         patch = fetchpatch {
+#           url = "https://patchwork.kernel.org/project/linux-arm-msm/patch/20251018-dpu-add-dspp-gc-driver-v1-1-ed0369214252@izzo.pro/mbox/";
+#           sha256 = "sha256-ez9nsrfhKGiP/YB9LsyRISYPDK1l9G8aqjnacCltQDE=";
+#         };
+#       }
+#       {
+#         name = "mac-address";
+#         patch = ./mac-address.patch;
+#       }
+#     ];
+#   })
 # linuxPackagesFor (
 #   (buildLinux {
 #     version = "6.19.6";
