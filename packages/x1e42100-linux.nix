be231bafc3d5f0e9d55be85aecca5b5e79a86142
Tomas Harkema <tomas@harkema.io>
2026-05-01 23:34:20 +0200

    readd update patch

--- packages/x1e42100-linux.nix
+++ packages/x1e42100-linux.nix
@@ -11,14 +11,26 @@ linuxPackagesFor (
   (buildLinux {
     version = "6.19.14";
 
-linuxPackagesFor (buildLinux {
+    src = fetchFromGitHub {
+      owner = "jglathe";
+      repo = "linux_ms_dev_kit";
+      rev = "jg/ubuntu-qcom-x1e-6.19.y";
+      sha256 = "sha256-g2AGoH36ayHn853yuiiiQRikN41c0I4Rgnto55IrTlE=";
+    };
 
-  src = fetchFromGitHub {
-    owner = "jglathe";
-    repo = "linux_ms_dev_kit";
-    rev = "jg/ubuntu-qcom-x1e-6.18.7-jg-0";
-    hash = "sha256-UMWBbby3BtA61NfwSfluXAXQpFCDmnYnID7IrjzIv6A=";
-  };
-  version = "6.18.7";
-
-})
+    kernelPatches = [
+      {
+        name = "dpu-add-dspp-gc-driver-v1-1-add-gamma";
+        patch = fetchpatch {
+          url = "https://patchwork.kernel.org/project/linux-arm-msm/patch/20251018-dpu-add-dspp-gc-driver-v1-1-ed0369214252@izzo.pro/mbox/";
+          sha256 = "sha256-ez9nsrfhKGiP/YB9LsyRISYPDK1l9G8aqjnacCltQDE=";
+        };
+      }
+      {
+        name = "mac-address";
+        patch = ./mac-address.patch;
+      }
+    ];
+  })
+   .override {stdenv = ccacheStdenv;}
+)
