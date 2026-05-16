f606de5140d99f2400da4189ec1e73a25ee528ae
firebat <66.firebat@gmail.com>
2026-05-16 09:55:31 -0600

    Fixed firmware paths in ./modules/firmware.nix. Harrison modified the DTS source and it now expects the firmware to be placed in a specific directory. He also mentioned that the firmware stored in his repo has been signed and we may be fetching from his remote in a future commit.

--- modules/firmware.nix
+++ modules/firmware.nix
@@ -7,7 +7,6 @@
     hash = "sha256-x5Y75WCzl3JfdrL4pPw1xgzY/z/qK+DFfrMo1bh8Eh4=";
   };
 in runCommand "qcom-laptops-firmware" {} ''
-  mkdir -p $out/lib/firmware/qcom/x1p42100/LENOVO/83HL
-  cp ${slim5x}/qcom/x1p42100/LENOVO/83HL/{adsp_dtbs.elf,adspr.jsn,adsps.jsn,adspua.jsn,battmgr.jsn,cdsp_dtbs.elf,cdspr.jsn,qcadsp8380.mbn,qcav1e8380.mbn,qccdsp8380.mbn,qcdxkmsuc8380.mbn,qcdxkmsucpurwa.mbn,qcvss8380.mbn,qcvss8380_pa.mbn} \
-    $out/lib/firmware/qcom/x1p42100/LENOVO/83HL/
+  mkdir -p $out/lib/firmware/qcom/x1p42100/Microsoft/Surface12
+   cp ${slim5x}/qcom/x1p42100/LENOVO/83HL/{adsp_dtbs.elf,adspr.jsn,adsps.jsn,adspua.jsn,battmgr.jsn,cdsp_dtbs.elf,cdspr.jsn,qcadsp8380.mbn,qcav1e8380.mbn,qccdsp8380.mbn,qcdxkmsuc8380.mbn,qcdxkmsucpurwa.mbn,qcvss8380.mbn,qcvss8380_pa.mbn} $out/lib/firmware/qcom/x1p42100/Microsoft/Surface12/
 ''
