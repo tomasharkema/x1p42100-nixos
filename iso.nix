01170eb2af547bf1d077ade8ca809567edb161b4
fireshark <66.firebat@gmail.com>
2026-05-26 17:21:37 -0600

    Added firmware as contents in the ISO image, to ensure that firmware is present on the live image as well. Verification can be performed by mounting and inspecting firmware locations:
    mount -o loop nixos-*.iso /mnt/iso
    ls -la /mnt/iso/lib/firmware/qcom/x1p42100/Microsoft/Surface12/

--- iso.nix
+++ iso.nix
@@ -12,9 +12,8 @@
   };
 
   hardware = {
-    enableAllHardware = lib.mkForce false; # true;
-
-    enableRedistributableFirmware = lib.mkForce false; # true;
+    enableAllHardware = lib.mkForce false;
+    enableRedistributableFirmware = lib.mkForce false;
     firmware = with pkgs; [
       linux-firmware
       wireless-regdb
@@ -22,9 +21,25 @@
     ];
   };
 
+  # Ensure firmware is copied to ISO root
+  environment.etc = {
+    "firmware".source = "${lib.makeSearchPath "lib/firmware" (with pkgs; [
+      linux-firmware
+      wireless-regdb
+      (pkgs.callPackage ./modules/firmware.nix {})
+    ])}";
+  };
+
   isoImage = {
     forceTextMode = true;
     squashfsCompression = "zstd";
+    # Ensure firmware files are included in the squashfs
+    contents = [
+      {
+        source = "${pkgs.callPackage ./modules/firmware.nix {}}/lib/firmware";
+        target = "/lib/firmware";
+      }
+    ];
   };
 
   environment.systemPackages = with pkgs; [
